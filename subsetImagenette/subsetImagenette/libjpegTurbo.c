2ec28f14c0d956e56c753ed6793d6a6f364f7e4e
Ayushi Tiwari <ayushitwari2103@gmail.com>
2020-06-16 09:34:59 -0400

    libjpegTurbo added

---
+++ subsetImagenette/subsetImagenette/libjpegTurbo.c
@@ -0,0 +1,9 @@
+//
+//  libjpegTurbo.c
+//  subsetImagenette
+//
+//  Created by Ayushi Tiwari on 2020-06-15.
+//  Copyright © 2020 Ayushi Tiwari. All rights reserved.
+//
+
+#include "libjpegTurbo.h"
